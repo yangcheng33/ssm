d6bb7651e78b25ce4df04efc9be37d4ef3faf724
yangchengvita <yangchengvita@didichuxing.com>
2017-11-07 16:31:53 +0800

    request log record

--- src/main/java/org/sample/ssm/web/filter/RequestLogFilter.java
+++ src/main/java/org/sample/ssm/web/filter/RequestLogFilter.java
@@ -1,6 +1,6 @@
 package org.sample.ssm.web.filter;
 
-import org.sample.ssm.common.constant.SessionConst;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,16 +12,14 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * 请求记录过滤器.
  */
-public class SessionFilter implements Filter {
-
-    protected Logger log = LoggerFactory.getLogger(this.getClass());
-
-    private FilterConfig filterConfig = null;
+public class RequestLogFilter implements Filter {
+    private final String[]     logHeaders   = { "PUT", "POST", "DELETE" };
+    protected     Logger       log          = LoggerFactory.getLogger(this.getClass());
+    private       FilterConfig filterConfig = null;
 
     public void init(FilterConfig filterConfig) throws ServletException {
         this.filterConfig = filterConfig;
@@ -40,21 +38,9 @@ public class SessionFilter implements Filter {
 
         HttpServletRequest httpRequest = (HttpServletRequest) request;
 
-        String servletPath = httpRequest.getServletPath();
-        if ("POST".equals(httpRequest.getMethod())) {
+        if (StringUtils.containsAny(httpRequest.getMethod(), logHeaders)) {
 
-            log.debug(">>> POST " + servletPath);
-            StringBuilder params = new StringBuilder();
-            for (Map.Entry<String, String[]> entry : request.getParameterMap().entrySet()) {
-                params.append(entry.getKey()).append("=");
-                if (entry.getValue().length > 0) {
-                    params.append(entry.getValue()[0]).append(" ");
-                } else {
-                    params.append(" ");
-                }
-            }
-            log.debug("\t参数: " + params.toString());
-            log.debug("\t操作者: " + ((HttpServletRequest) request).getSession().getAttribute(SessionConst.NAME));
+            log.debug(">>>{} {}", httpRequest.getMethod(), httpRequest.getServletPath());
         }
 
         //请求路径 URL
--- src/test/java/org/sample/ssm/ApplicationLoadTest.java
+++ src/test/java/org/sample/ssm/ApplicationLoadTest.java
@@ -1,6 +1,5 @@
 package org.sample.ssm;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -11,13 +10,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
- * test
+ * 加载spring配置的测试用例。
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring.xml", "file:src/main/webapp/WEB-INF/springmvc-servlet.xml" })
 @ActiveProfiles({ "dev" })
-// 标明是web应用测试
-@WebAppConfiguration(value = "src/main/webapp") //可以不填，默认此目录
+// 标明是web应用测试，默认此目录
+@WebAppConfiguration(value = "src/main/webapp")
 public class ApplicationLoadTest {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
